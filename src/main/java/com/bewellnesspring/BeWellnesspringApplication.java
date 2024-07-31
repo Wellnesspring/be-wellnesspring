3b2505a83bc7720e8693fa0ad124e13baf90c7bf
taewan <>
2024-07-31 22:09:06 +0900

    setting

---
+++ src/main/java/com/bewellnesspring/BeWellnesspringApplication.java
@@ -0,0 +1,13 @@
+package com.bewellnesspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeWellnesspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeWellnesspringApplication.class, args);
+    }
+
+}
