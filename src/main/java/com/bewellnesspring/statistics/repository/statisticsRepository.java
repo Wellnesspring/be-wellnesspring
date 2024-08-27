844b8543352f3f9e568493b3b58e2da268170d3a
Wigwangho <peach97089@gmail.com>
2024-08-27 18:20:31 +0900

    xml 쿼리 작성, 컨트롤러 및 서비스 형식 인터페이스로 변경

--- src/main/java/com/bewellnesspring/statistics/repository/statisticsRepository.java
+++ src/main/java/com/bewellnesspring/statistics/repository/statisticsRepository.java
@@ -1,43 +1,19 @@
 package com.bewellnesspring.statistics.repository;
 
-import lombok.extern.slf4j.Slf4j;
-import org.mybatis.spring.SqlSessionTemplate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Repository;
+import com.bewellnesspring.alert.model.vo.Alert;
+import org.apache.ibatis.annotations.Mapper;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-@Slf4j
-@Repository
-public class statisticsRepository {
-    Logger logger = LoggerFactory.getLogger(statisticsRepository.class);
+@Mapper
+public interface statisticsRepository {
 
-    private final SqlSessionTemplate sqlSessionTemplate;
-
-    public statisticsRepository(SqlSessionTemplate sqlSessionTemplate) {
-        this.sqlSessionTemplate = sqlSessionTemplate;
-    }
-
-    public List<Map<String, Object>> sport(Map<String, Object> rmap) {
-        List<Map<String, Object>> sportList = sqlSessionTemplate.selectList("selectSport", rmap);
-        logger.info(sportList.toString());
-        log.info("레포지토리");
-        return sportList;
-    }
-
-    public List<Map<String, Object>> kcal(Map<String, Object> rmap) {
-        List<Map<String, Object>> kcalList = sqlSessionTemplate.selectList("selectKcal", rmap);
-        logger.info(kcalList.toString());
-        log.info("레포지토리");
-        return kcalList;
-    }
-
-    public List<Map<String, Object>> food(Map<String, Object> rmap) {
-        List<Map<String, Object>> foodList = sqlSessionTemplate.selectList("selectFood", rmap);
-        logger.info(foodList.toString());
-        log.info("레포지토리");
-        return foodList;
-    }
+    List<Map<String,Object>> selectAll(Map<String,Object> rmap);
+    List<Map<String,Object>> selectSport(Map<String,Object> rmap);
+    List<Map<String,Object>> selectKcal(Map<String,Object> rmap);
+    List<Map<String,Object>> selectFood(Map<String,Object> rmap);
+    List<Map<String,Object>> selectLevel(Map<String,Object> rmap);
 }
