58fbcac1a00f618d86cbe32ef4a2c6a285a84d96
Wigwangho <peach97089@gmail.com>
2024-10-03 20:00:55 +0900

    meals 백엔드 개발(음식, 식사 구체사항 추가하는 기능)

--- src/main/java/com/bewellnesspring/meals/controller/mealsController.java
+++ src/main/java/com/bewellnesspring/meals/controller/mealsController.java
@@ -1,5 +1,8 @@
 package com.bewellnesspring.meals.controller;
 
+import com.bewellnesspring.statistics.controller.statisticsController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.bewellnesspring.meals.service.mealsService;
@@ -11,6 +14,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("dashboard/meals")
 public class mealsController {
+    private final Logger logger = LoggerFactory.getLogger(statisticsController.class);
 
     private final mealsService mealsService;
 
@@ -26,11 +30,19 @@ public class mealsController {
         rmap.put("user_id", user_id);
         return mealsService.getMealbyDate(rmap);
     }
-
+    //식사 상세정보 확인(음식)
+    @GetMapping("/getMealDetail")
+    public List<Map<String, Object>> getMealDetail(@RequestParam int meal_id) {
+        Map<String, Object> rmap = new HashMap<>();
+        rmap.put("meal_id", meal_id);
+        return mealsService.getMealDetail(rmap);
+    }
     // 음식 정보 조회
-    @GetMapping("/getFood")
-    public List<Map<String, Object>> getFood() {
-        return mealsService.getFood();
+    @GetMapping("/getFoodByString")
+    public List<Map<String, Object>> getFood(@RequestParam String searchTerm) {
+        Map<String, Object> rmap = new HashMap<>();
+        rmap.put("search_term", searchTerm);
+        return mealsService.getFoodByString(rmap);
     }
 
     // 영양소 정보 조회
@@ -43,17 +55,26 @@ public class mealsController {
 
     // 문자열 기반 식사 기록 조회
     @GetMapping("/getMealbyString")
-    public List<Map<String, Object>> getMealbyString(@RequestParam String meal_name, @RequestParam int user_id) {
+    public List<Map<String, Object>> getMealbyString(@RequestParam String meal_name, @RequestParam String user_id) {
         Map<String, Object> rmap = new HashMap<>();
         rmap.put("meal_name", meal_name);
         rmap.put("user_id", user_id);
         return mealsService.getMealbyString(rmap);
     }
 
+    // 좋아하는 음식
+    @GetMapping("/getFavorFood")
+    public List<Map<String, Object>> getFavorFood(@RequestParam String user_id) {
+        Map<String, Object> rmap = new HashMap<>();
+        rmap.put("user_id", user_id);
+        return mealsService.getFavorFood(rmap);
+    }
+
     // 식사 추가
     @PostMapping("/addMeal")
-    public void addMeal(@RequestParam String user_id, @RequestParam String meal) {
+    public void addMeal(@RequestParam(defaultValue = "userid_test") String user_id, @RequestParam String meal) {
         Map<String, Object> rmap = new HashMap<>();
+        logger.info(user_id, meal);
         rmap.put("user_id", user_id);
         rmap.put("meal", meal);
         mealsService.addMeal(rmap);
@@ -63,6 +84,9 @@ public class mealsController {
     @PostMapping("/addMealDetails")
     public void addMealDetails(@RequestParam int meal_id, @RequestParam int food_id, @RequestParam double amount) {
         Map<String, Object> rmap = new HashMap<>();
+        logger.info(String.valueOf(meal_id));
+        logger.info(String.valueOf(food_id));
+        logger.info(String.valueOf(amount));
         rmap.put("meal_id", meal_id);
         rmap.put("food_id", food_id);
         rmap.put("amount", amount);
@@ -71,20 +95,43 @@ public class mealsController {
 
     // 선호 음식 추가
     @PostMapping("/addFavor")
-    public void addFavor(@RequestParam int user_id, @RequestParam int food_id) {
+    public void addFavor(@RequestParam String user_id, @RequestParam int food_id) {
         Map<String, Object> rmap = new HashMap<>();
         rmap.put("user_id", user_id);
         rmap.put("food_id", food_id);
         mealsService.addFavor(rmap);
     }
 
-    // 식사 상세 정보 업데이트
-    @PostMapping("/updateMealDetail")
-    public void updateMealDetail(@RequestParam int meal_id, @RequestParam int food_id, @RequestParam double amount) {
+    // 식사명 업데이트
+    @PostMapping("/updateMeal")
+    public void updateMeal(@RequestParam String meal, @RequestParam int meal_id) {
         Map<String, Object> rmap = new HashMap<>();
         rmap.put("meal_id", meal_id);
-        rmap.put("food_id", food_id);
+        rmap.put("meal", meal);
+        mealsService.updateMeal(rmap);
+    }
+    //식사 상세정보 수정
+    @PostMapping("/updateMealDetail")
+    public void updateMeal(@RequestParam double amount, @RequestParam int id) {
+        Map<String, Object> rmap = new HashMap<>();
         rmap.put("amount", amount);
+        rmap.put("id", id);
         mealsService.updateMealDetail(rmap);
     }
+    //식사 detail 삭제
+    @PostMapping("/deleteMealDetail")
+    public void deleteMealDetail(@RequestParam int mealDetailId){
+        Map<String, Object> rmap = new HashMap<>();
+        rmap.put("mealDetailId", mealDetailId);
+        mealsService.deleteMealDetail(rmap);
+    }
+
+    @PostMapping("/deleteMeal")
+    public void deleteMeal(@RequestParam int meal_id){
+        Map<String, Object> rmap = new HashMap<>();
+        rmap.put("meal_id", meal_id);
+        mealsService.deleteMeal(rmap);
+    }
+
+
 }
--- src/main/java/com/bewellnesspring/meals/model/repository/mealsRepository.java
+++ src/main/java/com/bewellnesspring/meals/model/repository/mealsRepository.java
@@ -8,13 +8,23 @@ import java.util.Map;
 @Mapper
 public interface mealsRepository {
     List<Map<String, Object>> getMealbyDate(Map<String, Object> rmap);
-    List<Map<String, Object>> getFood();
+    List<Map<String, Object>> getFoodByString(Map<String, Object> rmap);
     List<Map<String, Object>> getNutrient(Map<String, Object> rmap);
     List<Map<String, Object>> getMealbyString(Map<String, Object> rmap);
     void addMeal(Map<String, Object> rmap);
     void addMealDetails(Map<String, Object> rmap);
     void addFavor(Map<String, Object> rmap);
-    void updateMealDetail(Map<String, Object> rmap);
+    void updateMeal(Map<String, Object> rmap);
 
     List<Map<String,Object>> getPlanbyDate(Map<String, Object> rmap);
+
+    List<Map<String,Object>> getMealDetail(Map<String, Object> rmap);
+
+    void deleteMealDetail(Map<String, Object> rmap);
+
+    void deleteMeal(Map<String, Object> rmap);
+
+    void updateMealDetail(Map<String, Object> rmap);
+
+    List<Map<String, Object>> getFavorFood(Map<String, Object> rmap);
 }
