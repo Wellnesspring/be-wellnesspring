d836af6c08c670faf8b04d006317cd32b9603a36
Wigwangho <peach97089@gmail.com>
2024-10-29 17:46:14 +0900

    컨트롤러 기능 정리

--- src/main/java/com/bewellnesspring/statistics/controller/statisticsController.java
+++ src/main/java/com/bewellnesspring/statistics/controller/statisticsController.java
@@ -31,8 +31,7 @@ public class statisticsController {
     private final statisticsService statisticsService;
 
     @GetMapping("")
-    public List<Map<String,Object>> allList(){
-        String id = "userid_test";
+    public List<Map<String,Object>> allList(@RequestParam String id){
         List<Map<String,Object>> allList = statisticsService.getAllData(id);
         logger.info(allList.toString());
         logger.info(id);
@@ -44,8 +43,8 @@ public class statisticsController {
     //일주일간 일별 운동 시간(총량)
     //지난주 운동 시간(일별)
     //운동 종류에 따라 본인 설정햔 운동 시간 목표
-    public List<Map<String,Object>> sportList(){
-        String id = "userid_test";
+    public List<Map<String,Object>> sportList(@RequestParam String id){
+
         List<Map<String,Object>> sportList = statisticsService.getSportData(id);
         logger.info(sportList.toString());
         return sportList;
@@ -53,8 +52,8 @@ public class statisticsController {
 
     @GetMapping("kcal")
     //일주일간 섭취한 칼로리, 소모한 칼로리, 각 일별 몸무게, 칼로리 섭취 목표량 및 소모 목표량    @GetMapping("kcal")
-    public List<Map<String,Object>> kcalList() {
-        String id = "userid_test";
+    public List<Map<String,Object>> kcalList(@RequestParam String id) {
+
         List<Map<String,Object>> kcalList = statisticsService.getKcalData(id);
         logger.info(kcalList.toString());
         return kcalList;
@@ -63,23 +62,20 @@ public class statisticsController {
 
     //칼로리, 나트륨, 단백질, 식이섬유, 탄수화물, 지방, 콜레스테롤
     @GetMapping("food")
-    public List<Map<String,Object>> foodList(){
-        String id = "userid_test";
+    public List<Map<String,Object>> foodList(@RequestParam String id){
         List<Map<String,Object>> foodList = statisticsService.getFoodData(id);
         logger.info(foodList.toString());
         return foodList;
     }
     @GetMapping("level")
-    public List<Map<String,Object>> levelList(){
-        String id = "userid_test";
+    public List<Map<String,Object>> levelList(@RequestParam String id){
         List<Map<String,Object>> levelList = statisticsService.getLevelData(id);
         logger.info(levelList.toString());
         return levelList;
     }
 
     @GetMapping("avg")
-    public List<Map<String,Object>> avgList(){
-        String id = "userid_test";
+    public List<Map<String,Object>> avgList(@RequestParam String id){
         List<Map<String,Object>> avgList = statisticsService.getAvgData(id);
         logger.info(avgList.toString());
         return avgList;
